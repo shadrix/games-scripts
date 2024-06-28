cbfe4f748ad4bfefb2e361f5c178a9c7622a3ba6
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:50:08 +0300

    Create bump.js

---
+++ bump.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bump
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://mmbump.pro/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/bump.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/bump.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
