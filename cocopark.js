958abe7954e2de084c16f9d48ece71e7ed500438
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:12:00 +0300

    Create cocopark.js

---
+++ cocopark.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         cocopark
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://miniapp.cocopark.io/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/cocopark.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/cocopark.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
