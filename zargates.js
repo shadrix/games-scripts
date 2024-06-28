3aabbf88308674adb0a58d8d24b54cc9b2247247
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:07:15 +0300

    Create zargates.js

---
+++ zargates.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         zargates
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://twa6-rd.zargates.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/zargates.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/zargates.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
