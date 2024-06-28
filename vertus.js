d0e879295d0689197327b49122efb895247e9ddb
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:53:43 +0300

    Create vertus.js

---
+++ vertus.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Vertus
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://thevertus.app/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/vertus.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/vertus.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
