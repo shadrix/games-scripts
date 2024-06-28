900eac75048ff5b084267e3631fe3f18e43d4168
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:10:00 +0300

    Create memetd.js

---
+++ memetd.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         memetd
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://play.memetd.game/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/memetd.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/memetd.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
