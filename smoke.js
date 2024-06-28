ff4091e1c14c0e04951775d2b43cd607e74fd27b
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:09:15 +0300

    Create smoke.js

---
+++ smoke.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         smoke
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://dev.smoke3.net/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/smoke.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/smoke.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
