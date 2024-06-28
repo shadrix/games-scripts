31a91f90bd73e115cd98968845e9ceb894804101
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:52:42 +0300

    Create hexn.js

---
+++ hexn.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Hexn
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://tgapp.hexn.io/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/hexn.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/hexn.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
