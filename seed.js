72e91d706dda8b8b4f1db0876ebf1a0a94f68e66
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:54:56 +0300

    Create seed.js

---
+++ seed.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Seed
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://cf.seeddao.org/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/seed.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/seed.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
