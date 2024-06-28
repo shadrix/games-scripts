00180abd4f916f15cd9a118634acde0f7acc4216
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:59:13 +0300

    Create epicofcastles.js

---
+++ epicofcastles.js
@@ -0,0 +1,12 @@
+
+// ==UserScript==
+// @name         Epic of castles
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://appv03.epicofcastles.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/epicofcastles.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/epicofcastles.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
