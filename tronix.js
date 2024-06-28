2af7a063b3e5f6021d265ae7238dcbfe8a643fe5
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:08:06 +0300

    Create tronix.js

---
+++ tronix.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         tronix
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://tronix.app/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/tronix.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/tronix.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
