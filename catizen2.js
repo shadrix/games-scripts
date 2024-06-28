745991c4ea09a0ad217c2b675cf754e5c18a556d
shadrix <postilha.dmytro@gmail.com>
2024-06-28 14:02:31 +0300

    Create catizen2.js

---
+++ catizen2.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Catizen 2
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://tgsvr.catizen.ai/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/catizen2.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/catizen2.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
