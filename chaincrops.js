23f58bb4aa573dad7922090ec7df4398a5db06eb
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:13:39 +0300

    Create chaincrops.js

---
+++ chaincrops.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bump
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        hhttps://app.chaincrops.io/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/chaincrops.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/chaincrops.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
