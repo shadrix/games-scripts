86c5fe200b2c05ffae97479db2727c48e3105a60
shadrix <postilha.dmytro@gmail.com>
2024-06-28 15:14:38 +0300

    Create poplaunch.js

---
+++ poplaunch.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         poplaunch
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://play.poplaunch.io/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/poplaunch.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/poplaunch.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
