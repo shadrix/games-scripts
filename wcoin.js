90aad3b9e751ee37f521e46b78a508b22e3ab457
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:51:36 +0300

    Create wcoin.js

---
+++ wcoin.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         W-coin
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://alohomora-bucket-fra1-prod-frontend-static.fra1.cdn.digitaloceanspaces.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/wcoin.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/wcoin.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
