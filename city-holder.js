0ffa0d404fe237916d11fee8c7493cb84508a861
shadrix <postilha.dmytro@gmail.com>
2024-06-28 13:58:06 +0300

    Create city-holder.js

---
+++ city-holder.js
@@ -0,0 +1,12 @@
+
+/// ==UserScript==
+// @name         City holder
+// @version      1.1
+// @namespace    Violentmonkey Scripts
+// @author       shadrix
+// @match        https://homyak.city-holder.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/shadrix/games-scripts/main/city-holder.js
+// @updateURL    https://raw.githubusercontent.com/shadrix/games-scripts/main/city-holder.js
+// @homepage     https://github.com/shadrix/games-scripts
+// ==/UserScript==
